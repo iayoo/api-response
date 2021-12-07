62fb24167182f96b135eeb14a24f29b6027fee59
Iayoo <vilnius94@163.com>
2021-12-07 15:01:51 +0800

    # bug Larvel 拦截修复

--- src/Exception/LumenHandle.php
+++ src/Exception/LumenHandle.php
@@ -18,7 +18,6 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
-use Illuminate\Support\Arr;
 
 
 class LumenHandle extends ExceptionHandler
