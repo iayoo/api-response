0a19dcf7c18cc217d7a72f7165d37cde2d9d0888
Iayoo <vilnius94@163.com>
2021-12-28 13:50:50 +0800

    # 新增thinkphp支持

---
+++ src/Response/ThinkPHP/ResponseTrait.php
@@ -0,0 +1,153 @@
+<?php
+
+
+namespace Iayoo\ApiResponse\Response\ThinkPHP;
+
+use think\Response;
+use think\response\Json;
+
+/**
+ * Trait ResponseTrait
+ * 响应结构说明
+ * {
+ *     staut   // 描述 HTTP 响应结果：HTTP 状态响应码在 500-599 之间为”fail”，在 400-499 之间为”error”，其它均为”success”
+ *     code    // 业务描述操作码，比如 200001 表示注册成功
+ *     message // 响应描述
+ *     data    // 实际的响应数据
+ *     trace   // 异常时的调试信息
+ * }
+ * @package Iayoo\ApiResponse\Response\Laravel
+ */
+trait ResponseTrait
+{
+    /** @var int HTTP 状态码 */
+    protected $httpStatusCode = 200;
+    /** @var string success|fail|error */
+    protected $statusCode = 'success';
+    /** @var int 业务错误骂  */
+    protected $errorCode = 0;
+
+    protected $isLayer = false;
+
+    /**
+     * @param int $httpStatusCode
+     */
+    public function setHttpStatusCode($httpStatusCode)
+    {
+        $this->httpStatusCode = $httpStatusCode;
+        return $this;
+    }
+
+    /**
+     * @param string $statusCode
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+        return $this;
+    }
+
+    /**
+     * @param int $errorCode
+     */
+    public function setErrorCode($errorCode)
+    {
+        $this->errorCode = $errorCode;
+        return $this;
+    }
+
+    /**
+     * @param bool $isLayer
+     */
+    public function setIsLayer(bool $isLayer)
+    {
+        $this->isLayer = $isLayer;
+        return $this;
+    }
+
+    /**
+     * 成功响应
+     * @param string|array $message 响应说明
+     * @param array $data 响应数据
+     * @return JsonResponse
+     */
+    public function success($message = 'success', $data = [])
+    {
+        if (is_array($message)) {
+            $data    = $message;
+            $message = 'success';
+        }
+        $this->setStatusCode('success');
+        return $this->response($message,$data);
+    }
+
+    /**
+     * 通常为代码异常等
+     * 状态响应码在 500-599 之间为”fail”
+     * @param $message
+     * @param array $trace
+     * @return JsonResponse
+     */
+    public function fail($message,$trace = []){
+        $this->setStatusCode('fail');
+        return $this->response($message,[],$trace);
+    }
+
+    /**
+     * 用户操作等错误
+     * @param $message
+     * @param int $code 业务错误码
+     *
+     * @return JsonResponse
+     */
+    public function error($message, $code = 40000,$errorData = [])
+    {
+        $this->setStatusCode('error');
+        $this->setErrorCode($code);
+        return $this->response($message,[]);
+    }
+
+    public function exception($request, $exception)
+    {
+
+        $status = method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 40000;
+
+        return new JsonResponse(
+            [
+                'message' => $exception->getMessage(),
+                'status'  => $this->statusCode,
+                'data'    => $this->convertExceptionToArray($exception),
+            ],
+            $this->httpStatusCode,
+            $this->isHttpException($exception) ? $exception->getHeaders() : [],
+            JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
+        );
+    }
+
+    protected function response($message,$data,$trace = [])
+    {
+        $status = $this->statusCode;
+        $code = $this->errorCode;
+        if ($status === 'fail'){
+            $message = env('APP_DEBUG')?"系统异常:{$message}":"系统异常";
+        }
+        if ($status === 'success'){
+            $trace = [];
+        }
+        if ($this->isLayer){
+            $data['code'] = $status === 'success'?0:-1;
+            return Response::create(
+                $status === 'success'
+                    ?$data
+                    :compact('message','status','code','data','trace'),
+                'json',
+                $this->httpStatusCode);
+        }
+        return Response::create(
+            $status === 'success'
+                ?compact('message','status','code','data')
+                :compact('message','status','code','data','trace'),
+            'json',
+            $this->httpStatusCode);
+    }
+}
